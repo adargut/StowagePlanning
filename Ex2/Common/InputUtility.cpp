53ac035534b7d339404e5a5979b968cf39ccefd7
adargut <adar276@gmail.com>
2020-05-10 17:07:36 -0700

    Some progress to command line

--- Ex2/Common/InputUtility.cpp
+++ Ex2/Common/InputUtility.cpp
@@ -1,20 +1,112 @@
-//
-// Created by Adar on 10/05/2020.
-//
-
 #include "InputUtility.h"
 
-bool InputUtility::cmdOptionExists(char** begin, char** end, const std::string& option)
+
+bool handleTravelArg(const string& travel_path)
 {
-    return std::find(begin, end, option) != end;
+    if (!fs::exists(travel_path))
+    {
+        // TODO return fatal error and make sure travel stops
+        return false;
+    }
+
+    // Check if travel path contains both route and plan files
+    string route_file, plan_file;
+    for (const auto& entry : directory_iterator(travel_path))
+    {
+        string file_path = entry.path();
+        if (boost::algorithm::ends_with(file_path, ROUTE_SUFFIX))
+        {
+            //TODO parse route file
+            route_file = file_path;
+        }
+        else if (boost::algorithm::ends_with(file_path, PLAN_SUFFIX))
+        {
+            //TODO parse plan file
+            plan_file = file_path;
+        }
+    }
+    if (plan_file.empty() || plan_file.empty())
+    {
+        //TODO fatal error
+        return false;
+    }
+
+    for (const auto& entry : directory_iterator(travel_path))
+    {
+        string file_path = entry.path();
+        if (boost::algorithm::ends_with(file_path, CARGO_SUFFIX))
+        {
+            // TODO handle cargo found
+        }
+
+    }
+
+    return true;
 }
 
-char *InputUtility::getCmdOption(char **begin, char **end, const std::string &option)
+bool handleAlgorithmArg(const string& algorithm_path)
 {
-    char ** itr = std::find(begin, end, option);
-    if (itr != end && ++itr != end)
+    return true;
+}
+
+bool handleAlgorithmArg()
+{
+    return true;
+}
+
+bool handleOutputArg(const string& algorithm_path)
+{
+    return true;
+}
+
+
+bool handleArgs(int argc, char** argv)
+{
+    // Declare the supported options
+    po::options_description desc("Allowed options");
+    desc.add_options()
+            (TRAVEL_OPTION, po::value<string>(), TRAVEL_DESC)
+            (OUTPUT_OPTION, po::value<string>(), OUTPUT_DESC)
+            (ALGORITHM_OPTION, po::value<string>(), ALGORITHM_DESC);
+
+    // Store the command line arguments in a variable map
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv)
+                      .options(desc)
+                      .style(po::command_line_style::unix_style | po::command_line_style::allow_long_disguise)
+                      .run(), vm);
+    po::notify(vm);
+
+    // Parse path to travel folder
+    if (vm.count(TRAVEL_OPTION))
     {
-        return *itr;
+        handleTravelArg(vm[TRAVEL_OPTION].as<string>());
     }
-    return nullptr;
+    else
+    {
+        // TODO return fatal error because no travel supplied and stop travel
+        return false;
+    }
+
+    // Parse path to algorithm folder
+    if (vm.count(ALGORITHM_OPTION))
+    {
+        handleAlgorithmArg(vm[ALGORITHM_OPTION].as<string>());
+    }
+    else
+    {
+        handleAlgorithmArg();
+    }
+
+    // Parse path to output folder
+    if (vm.count(OUTPUT_OPTION))
+    {
+        handleOutputArg(vm[OUTPUT_OPTION].as<string>());
+    }
+    else
+    {
+        handleAlgorithmArg();
+    }
+
+    return true;
 }
--- Ex2/Common/InputUtility.h
+++ Ex2/Common/InputUtility.h
@@ -1,17 +1,41 @@
-//
-// Created by Adar on 10/05/2020.
-//
-
 #ifndef EX2_INPUTUTILITY_H
 #define EX2_INPUTUTILITY_H
 
-#include <algorithm>
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/program_options.hpp>
+#include <filesystem>
 #include <iostream>
 
+#define TRAVEL_OPTION "travel_path"
+#define TRAVEL_DESC "path to travel"
+#define ALGORITHM_OPTION "-algorithm_path"
+#define ALGORITHM_DESC "path to algorithm"
+#define OUTPUT_OPTION "-output_path"
+#define OUTPUT_DESC "path to output"
+#define ROUTE_SUFFIX ".route"
+#define PLAN_SUFFIX ".ship_plan"
+#define CARGO_SUFFIX ".cargo_data"
 
-namespace InputUtility {
-    char* getCmdOption(char ** begin, char ** end, const std::string& option);
-    bool cmdOptionExists(char** begin, char** end, const std::string& option);
+namespace fs = std::filesystem;
+namespace po = boost::program_options;
+using string = std::string;
+using std::cout;
+using directory_iterator = fs::directory_iterator;
+
+
+namespace InputUtility
+{
+    bool handleTravelArg(const string& travel_path);
+
+    bool handleAlgorithmArg(const string& algorithm_path);
+
+    bool handleAlgorithmArg();
+
+    bool handleOutputArg(const string& algorithm_path);
+
+    bool handleOutputArg();
+
+    bool handleArgs(int argc, char** argv);
 }
 
 
