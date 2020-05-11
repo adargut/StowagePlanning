8bf9ad87eca1fe168c587b50fcac6d6d3d485a0d
adargut <adar276@gmail.com>
2020-05-10 17:08:17 -0700

    Some progress to command line

--- Ex2/Simulator/main.cpp
+++ Ex2/Simulator/main.cpp
@@ -1,13 +1,4 @@
 #include "../Common/InputUtility.h"
-#include <filesystem>
-#include <boost/algorithm/string/predicate.hpp>
-#include <boost/program_options.hpp>
-
-
-namespace fs = std::filesystem;
-using string = std::string;
-using std::cout;
-using directory_iterator = fs::directory_iterator;
 
 
 int main()
