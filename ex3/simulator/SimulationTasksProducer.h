102478e18e1dc01f24122a0799c265d3a6cfdcde
adargut <adar276@gmail.com>
2020-06-10 18:30:18 -0400

    Refactor SimulationTasksProducer.h

--- ex3/simulator/SimulationTasksProducer.h
+++ ex3/simulator/SimulationTasksProducer.h
@@ -6,11 +6,13 @@
 
 class SimulationTasksProducer : public AbstractTasksProducer
 {
-    ProcessedDataList m_processed_travels; // the tasks
+    const ProcessedDataList &m_processed_travels; // the tasks
     AlgorithmTravelResultsMap &m_results; // where task results are stored
 public:
-    SimulationTasksProducer(int numTasks, ProcessedDataSingleton &travel_data) : AbstractTasksProducer(numTasks), m_travel_data(travel_data) {}
-    SimulationTasksProducer(SimulationTasksProducer && other) : AbstractTasksProducer(std::move(other)), m_travel_data(other.m_travel_data), m_results(other.m_results) {}
+    SimulationTasksProducer(int numTasks, ProcessedDataList &processed_travels, AlgorithmTravelResultsMap &results) :
+    AbstractTasksProducer(numTasks), m_processed_travels(processed_travels), m_results(results) {}
+    SimulationTasksProducer(SimulationTasksProducer && other) : AbstractTasksProducer(std::move(other)),
+    m_processed_travels(other.m_processed_travels), m_results(other.m_results) {}
     std::optional<std::function<void(void)>> getTask() override;
 };
 
